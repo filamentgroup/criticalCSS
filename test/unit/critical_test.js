fa042d6bf52c59395d925a4e4a3815c5bf06dd2e
Jeffrey Lembeck <jlembeck@gmail.com>
2014-10-13 19:43:02 -0700

    Test that reproduces the problem for #13
    Test that reproduces the problem for #13
    
    Test for #13

--- test/unit/critical_test.js
+++ test/unit/critical_test.js
@@ -173,6 +173,14 @@
 				}
 				test.done();
 			});
+		},
+		"opera dpi rules": function( test ){
+			critical.getRules(path.resolve(path.join(__dirname, "..", "files", "dpi.css")), function( err, content ){
+				if( err ){ throw err; }
+
+				test.equal( content, "[{\"media\":{\"0\":\"only screen and (-o-min-device-pixel-ratio: 2/1)\",\"length\":1,\"mediaText\":\"only screen and (-o-min-device-pixel-ratio: 2/1)\"},\"cssText\":\"@media only screen and (-o-min-device-pixel-ratio: 2/1) { \\n  div { margin-left: 20px; }\\n}\",\"cssRules\":[{\"cssText\":\"div { margin-left: 20px; }\",\"selectorText\":\"div\",\"cssRules\":[]}]}]" );
+				test.done();
+			});
 		}
 	};
 }(typeof exports === "object" && exports || this));
