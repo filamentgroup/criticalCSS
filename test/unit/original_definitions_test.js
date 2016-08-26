5a80d134481a12cd426efcf66a151e2587d2ba79
John Bender <john.m.bender@gmail.com>
2016-08-26 15:33:10 -0400

    more tests

--- test/unit/original_definitions_test.js
+++ test/unit/original_definitions_test.js
@@ -7,18 +7,37 @@
 	var critical = require(path.join( "..", "..", "critical.js") );
 
 	exports.installOriginalDefinitions = {
-		setUp: function(done) {
-			// setup here
-			done();
-		},
-		tearDown: function( done ){
-			done();
-		},
 		"adds stripped definitions": function(test) {
 			test.expect(1);
 			// tests here
-			var originalCSS = "body { color: red; } div {}";
-			var criticalCSS = "body { color: blue; }";
+			var originalCSS = "body { color: red; }";
+			var criticalCSS = "body {}";
+
+			var result = critical
+						.installOriginalDefinitions(originalCSS, criticalCSS, { compress: true });
+
+			test.equal(result, "body{color:red;}");
+			test.done();
+		},
+
+		"adds muliple stripped definitions": function(test) {
+			test.expect(1);
+			// tests here
+			var originalCSS = "body { color: red; font-size: 20px; }";
+			var criticalCSS = "body {}";
+
+			var result = critical
+						.installOriginalDefinitions(originalCSS, criticalCSS, { compress: true });
+
+			test.equal(result, "body{color:red;font-size:20px;}");
+			test.done();
+		},
+
+		"does not include removed selectors": function(test) {
+			test.expect(1);
+			// tests here
+			var originalCSS = "body { color: red; } div.removed {}";
+			var criticalCSS = "body {}";
 
 			var result = critical
 						.installOriginalDefinitions(originalCSS, criticalCSS, { compress: true });
