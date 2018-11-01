fed68596756a85d32a04defc32998d93228bf3a4
John Bender <john.m.bender@gmail.com>
2018-11-01 17:36:54 -0400

    promise manual test

--- test/manual.js
+++ test/manual.js
@@ -4,12 +4,23 @@ criticalcss.getRules("test/files/all.css", function(err, output) {
   if (err) {
     throw new Error(err);
   } else {
-    criticalcss.findCritical("http://107.170.208.194:8080/test/files/test-site.html", { rules: JSON.parse(output) }, function(err, output) {
+    criticalcss.findCritical("http://107.170.208.194:8080/test-site-print.html", { rules: JSON.parse(output) }, function(err, output) {
+
       if (err) {
         throw new Error(err);
       } else {
-        console.log(output);
+        console.log("output:", output);
       }
     });
+  }
+});
+
+criticalcss.getRules("test/files/all.css", function(err, output) {
+  if (err) {
+    throw new Error(err);
+  } else {
+    criticalcss
+      .findCritical("http://107.170.208.194:8080/test-site-print.html", { rules: JSON.parse(output) })
+      .then((critCSS) => console.log("promise output:", critCSS));
   }
 });
